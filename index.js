d626a6ed6abcd5586105852965ef5d094c5088de
Sahana Koli <sahanasultanakoli@gmail.com>
2023-10-18 23:21:52 +0600

    delete a product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,9 +28,16 @@ async function run() {
     await client.connect();
 
     const brandCollection = client.db('brandDB').collection('brand');
+    const productCollection = client.db('productDB').collection('product');
 
     app.get('/brand', async(req, res) => {
       const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/product', async(req,res) =>{
+      const cursor = productCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
@@ -39,6 +46,20 @@ async function run() {
       const brandItem = req.body;
       console.log(brandItem);
       const result = await brandCollection.insertOne(brandItem);
+      res.send(result);
+    })
+
+    app.post('/product', async(req, res) => {
+      const productItem = req.body;
+      console.log(productItem);
+      const result = await productCollection.insertOne(productItem);
+      res.send(result);
+    })
+
+    app.delete('/product/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await productCollection.deleteOne(query);
       res.send(result);
     })
 
